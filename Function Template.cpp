9fb8e7e869a608650621b4b4000408ae452c7e96
beykiin <132366582+beykiin@users.noreply.github.com>
2024-02-18 13:58:53 +0300

    Add files via upload

---
+++ Function Template.cpp
@@ -0,0 +1,113 @@
+﻿// Fonksiyon Şablonu.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+//template<typename T>
+//T max(T a, T b) {
+//    return(a > b) ? a : b;
+//
+//}
+//
+//
+//
+//int main()
+//{
+//    std::cout << max(7.1, 9.2);
+//    std::cout << max<int>(7, 9);
+//}
+
+//template<typename T>
+//T max(T a, T b) {
+//    return(a > b) ? a : b;
+//
+//}
+//
+//
+//
+//int main()
+//{
+//    std::cout << max(7.1, 9.2);
+//    std::cout << max<>(7, 9);
+//}
+
+//template<typename T>
+//T max(T a, T b) {
+//    return(a > b) ? a : b;
+//
+//}
+//
+//
+//
+//int main()
+//{
+//    std::cout << max(7.1, 9.2);
+//    std::cout << max(7, 9);
+//}
+
+//template<typename T>
+//T max(T a, float b) {
+//    return(a > b) ? a : b;
+//
+//}
+//
+//
+//
+//int main()
+//{
+//    std::cout << max(7.1, 9.2);
+//    std::cout << max(7, 9.2);
+//}
+
+//template<typename T>
+//T max(int a, int b) {
+//    return(a > b) ? a : b;
+//
+//}
+//
+//
+//
+//int main()
+//{
+//    
+//    std::cout << max <int>(11,9);
+//}
+
+//template<typename T, typename U>
+//T max(T a, U b) {
+//    return(a > b) ? a : b;
+//
+//}
+//
+//
+//
+//int main()
+//{
+//
+//    std::cout << max <>(11, 9);
+//}
+
+//template<typename T, typename U, typename L>
+//T max(T a, U b, L j) {
+//    return(a > b) ? a : b;
+//
+//}
+//
+//
+//
+//int main()
+//{
+//
+//    std::cout << max <int, float, double>(11, 9.2, 0.5554455);
+//}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
